f997287cd046a0824e0a93389a875f70c966ba71
Adriano Berto <adriano.berto@outlook.com>
2024-02-06 14:01:53 -0300

    retorna em bytes

--- PDFPW/Seguranca.cs
+++ PDFPW/Seguranca.cs
@@ -157,3 +157,57 @@ namespace PDFPW
 //    }
 //}
 
+
+
+
+
+
+//retorna em bytes
+
+//class Program
+//{
+//    static void Main(string[] args)
+//    {
+//        // Caminho para o arquivo PDF existente
+//        string pdfFilePath = "existing_file.pdf";
+
+//        // Senha para o usuário e senha do proprietário
+//        string userPassword = "userPassword";
+//        string ownerPassword = "ownerPassword";
+
+//        // Permissões do PDF (somente leitura)
+//        var permissions = EncryptionConstants.ALLOW_PRINTING;
+
+//        // Aplicar proteção por senha com permissões restritas ao PDF existente
+//        byte[] protectedPdfBytes = ApplyPdfPasswordProtection(pdfFilePath, userPassword, ownerPassword, permissions);
+
+//        // Escrever os bytes do arquivo PDF protegido em um novo arquivo
+//        File.WriteAllBytes("output_protected.pdf", protectedPdfBytes);
+
+//        Console.WriteLine("PDF protegido como somente leitura com sucesso!");
+//    }
+
+//    static byte[] ApplyPdfPasswordProtection(string inputPdfPath, string userPassword, string ownerPassword, int permissions)
+//    {
+//        using (MemoryStream outputStream = new MemoryStream())
+//        {
+//            // Abrir o PDF existente
+//            using (var pdfReader = new PdfReader(inputPdfPath))
+//            using (var pdfWriter = new PdfWriter(outputStream))
+//            {
+//                // Configurar a proteção por senha
+//                var encryptionProperties = new PdfEncryption();
+//                encryptionProperties.SetUserPassword(System.Text.Encoding.UTF8.GetBytes(userPassword));
+//                encryptionProperties.SetOwnerPassword(System.Text.Encoding.UTF8.GetBytes(ownerPassword));
+//                encryptionProperties.AddPermissions(permissions);
+
+//                // Aplicar a proteção por senha ao PDF
+//                var pdfDocument = new PdfDocument(pdfReader, pdfWriter);
+//                pdfDocument.SetEncryption(encryptionProperties);
+//            }
+
+//            // Obter os bytes do MemoryStream
+//            return outputStream.ToArray();
+//        }
+//    }
+//}
